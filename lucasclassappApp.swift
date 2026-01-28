17ab98f4c706cf81db9855517e8228e868c7b6eb
Sudhindran Senthilkumar <Sudhin18oct@hotmail.com>
2026-01-28 16:05:56 -0500

    Add files via upload

---
+++ lucasclassappApp.swift
@@ -0,0 +1,17 @@
+//
+//  lucasclassappApp.swift
+//  lucasclassapp
+//
+//  Created by Sudhindran Senthilkumar on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct lucasclassappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
